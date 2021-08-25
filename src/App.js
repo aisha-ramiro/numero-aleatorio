0981df3be6f109f8071bb69c07a654724682cb04
Aisha Ramiro <aishamontealto@gmail.com>
2021-08-25 11:44:14 -0300

    commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import PaginaInicial from './componentes/pagina-inicial';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <PaginaInicial />
+    </div>
+  );
+}
+
+export default App;
